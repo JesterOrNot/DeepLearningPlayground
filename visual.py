7817a13b1c82590e30d91812107e7400cfebb53a
Sean Hellum <seanhellum45@gmail.com>
2020-05-20 02:39:58 +0000

    Add comments

--- visual.py
+++ visual.py
@@ -1,7 +1,10 @@
-from keras.models import Sequential
+from ann_visualizer.visualize import ann_viz
 from keras.layers import Dense
-from ann_visualizer.visualize import ann_viz;
+from keras.models import Sequential
 
+# Input Layer: 3 Nodes
+# Hidden Layer: 4 Nodes
+# Output Layer: 2 Nodes
 model = Sequential([
     Dense(4, input_shape=(3,)),
     Dense(2)
